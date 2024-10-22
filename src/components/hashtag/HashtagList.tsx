afa671647705cdba3dfac692e390be6e2fbd2d7f
temidayo david <isitemidayo@gmail.com>
2024-10-22 16:32:32 +0100

    first commit with context api

---
+++ src/components/hashtag/HashtagList.tsx
@@ -0,0 +1,20 @@
+import { useFeedbackItemsContext } from "../../lib/hooks";
+import HashtagItem from "./HashtagItem";
+
+export default function HashtagList() {
+  const { companyList, handleSelectCompany } = useFeedbackItemsContext();
+
+  return (
+    <ul className="hashtags">
+      {companyList.map((company) => {
+        return (
+          <HashtagItem
+            key={company}
+            company={company}
+            onSelectCompany={handleSelectCompany}
+          />
+        );
+      })}
+    </ul>
+  );
+}
